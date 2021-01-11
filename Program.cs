ef994d41f4615c7cdbbbeb42f7e85292ebb13f5c
KevalPrajapati <keval.prajapatimus@gmail.com>
2021-01-11 20:27:30 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] names = {"keval","aditya","dev","dhinchak","pooja"};
+            Console.WriteLine("Hello Franz chai peelo!! \nonly Indians understand");
+            foreach(String name in names){
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
